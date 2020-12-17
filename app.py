46dbc800ba26c07f3d50e184f3340c21cec002cb
deog.amani@gmail.com <deog.amani@gmail.com>
2020-12-17 11:45:01 +0200

    testing

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 from tensorflow.keras.models import load_model
 
-# ratings_df = pd.read_csv("D:/School Related Documents and Apps/4th Year/Machine Learning/books-host/book_recommender_model_deployment/dataset/ratings.csv")
-# books_df = pd.read_csv("D:/School Related Documents and Apps/4th Year/Machine Learning/books-host/book_recommender_model_deployment/dataset/books.csv")
+ratings_df = pd.read_csv("D:/School Related Documents and Apps/4th Year/Machine Learning/books-host/book_recommender_model_deployment/dataset/ratings.csv")
+books_df = pd.read_csv("D:/School Related Documents and Apps/4th Year/Machine Learning/books-host/book_recommender_model_deployment/dataset/books.csv")
 
 st.title("Books Recommender System")
 html_temp = """
@@ -19,16 +19,16 @@ html_temp = """
 	"""
 st.markdown(html_temp,unsafe_allow_html=True)
 
-# st.dataframe(ratings_df)
+st.dataframe(ratings_df)
 
-# b_id =list(ratings_df.book_id.unique())
-# b_id.remove(10000)
-# book_arr = np.array(b_id)
+b_id =list(ratings_df.book_id.unique())
+b_id.remove(10000)
+book_arr = np.array(b_id)
 
 
 
-# user_input =st.number_input("Enter user Id here for books recommendations",1, len(b_id),1)
-# st.write("***Input***" , user_input)
+user_input =st.number_input("Enter user Id here for books recommendations",1, len(b_id),1)
+st.write("***Input***" , user_input)
 
 
 # st.write(len(user))
@@ -39,10 +39,10 @@ model = load_model( 'model.h5', compile = False )
 # model = load_model(model)
 
 
-# if st.button("Predict"):
-# 	user = np.array([user_input for i in range(len(b_id))])
-# 	pred = model.predict([book_arr, user])
-# 	pred = pred.reshape(-1)
-# 	pred_ids = (-pred).argsort()[0:5]
+if st.button("Predict"):
+	user = np.array([user_input for i in range(len(b_id))])
+	pred = model.predict([book_arr, user])
+	pred = pred.reshape(-1)
+	pred_ids = (-pred).argsort()[0:5]
 
-# 	st.write(books_df.iloc[pred_ids].title)
+	st.write(books_df.iloc[pred_ids].title)
